723a23effffc8958b3ff92bfe19cc9dee1e676c3
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 18:05:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+ programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+ programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 end
 
 def changing_alan
